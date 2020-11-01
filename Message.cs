e84ff26966daff0f9a96ef173cb9359afc8382b9
Vstro <64423882+Vstro@users.noreply.github.com>
2020-11-01 20:28:19 +0300

    Init commit

---
+++ Message.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Lab9_RegEx
+{
+    class Message
+    {
+        public String Value { get; set; }
+
+        public Message(String value = "")
+        {
+            Value = value;
+        }
+
+        public bool Contains(String word)
+        {
+            return Regex.IsMatch(Value, @"\b" + word + @"\b", RegexOptions.IgnoreCase);
+        }
+
+        public List<String> GetWordsWithLength(int length)
+        {
+            List<String> words = new List<string>();
+            foreach (Match match in Regex.Matches(Value, @"\b\w{" + length + "," + length + @"}\b"))
+            {
+                words.Add(match.Value);
+            }
+            return words;
+        }
+
+        public void DeleteSingleLetterWords()
+        {
+            Value = Regex.Replace(Value, @"\b\w\b", "");
+        }
+
+        public void DeleteRussianFirstVowelWords()
+        {
+            Value = Regex.Replace(Value, @"\b[ёуеыаоэяию]\w*\b", "", RegexOptions.IgnoreCase);
+        }
+
+        public void ReplaceEnglishWordsByEllipsis()
+        {
+            Value = Regex.Replace(Value, @"\b[a-z]+\b", "...", RegexOptions.IgnoreCase);
+        }
+
+        public double SumUpAllNumbers()
+        {
+            double sum = 0;
+            foreach (Match match in Regex.Matches(Value, @"\b\d+(,\d+)?\b"))
+            {
+                sum += double.Parse(match.Value);
+            }
+            return sum;
+        }
+
+        public List<String> GetPhoneNumbers()
+        {
+            List<String> numbers = new List<string>();
+            foreach (Match match in Regex.Matches(
+                Value, @"(\d{3}\-\d{3})|(\d{2}\-\d{2}\-\d{2})|(\d{3}\-\d{2}\-\d{2})"))
+            {
+                numbers.Add(match.Value);
+            }
+            return numbers;
+        }
+
+        public List<String> GetCurrentYearDates()
+        {
+            int currentYear = DateTime.Now.Year;
+            List<String> dates = new List<string>();
+            foreach (Match match in Regex.Matches(
+                Value, @"\b([12]?\d|3[01])\.([1-9]|1[0-2])\." + currentYear + @"\b"))
+            {
+                dates.Add(match.Value);
+            }
+            return dates;
+        }
+
+        public List<String> GetWebAdresses()
+        {
+            List<String> adresses = new List<string>();
+            foreach (Match match in Regex.Matches(Value, @"\bwww(\.\w+){2,}\b"))
+            {
+                adresses.Add(match.Value);
+            }
+            return adresses;
+        }
+
+        public void RoundSecInTime()
+        {
+            Value = Regex.Replace(
+                Value,
+                @"\b([01]\d|2[0-3])(:[0-5]\d){2}\b",
+                (match) => 
+                {
+                    int hour = int.Parse(match.Value.Substring(0, 2));
+                    int min = int.Parse(match.Value.Substring(3, 2));
+                    int sec = int.Parse(match.Value.Substring(6, 2));
+                    if (sec >= 30)
+                    {
+                        hour = (hour + (min + 1) / 60) % 24;
+                        min = (min + 1) % 60;
+                    } 
+                    return string.Format("{0:00}:{1:00}", hour, min);
+                });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab9_RegEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char choice = ' ';
+            Message message = new Message();
+            String buffer;
+            Console.WriteLine("*Text Processing With RegEx*");
+            do
+            {
+                Console.WriteLine("\nВыберите пункт меню:\n" +
+                    "1) Cодержится ли в сообщении заданное слово.\n" +
+                    "2) Вывести все слова заданной длины.\n" +
+                    "3) Удалить из сообщения все однобуквенные слова.\n" +
+                    "4) Удалить из сообщения русские слова, которые начинаются на гласную букву.\n" +
+                    "5) Заменить все английские слова на многоточие.\n" +
+                    "6) Найти сумму всех имеющихся в сообщении чисел (целых и вещественных).\n" +
+                    "7) Вывести все номера телефонов (xx-xx-xx, xxx-xxx или xxx-xx-xx), которые содержатся в сообщении.\n" +
+                    "8) Вывести все даты, которые относятся к текущему году.\n" +
+                    "9) Вывести все адреса web-сайтов, содержащиеся в сообщении.\n" +
+                    "0) Преобразовать время в формате чч:мм:сс к формату чч:мм.\n" +
+                    "E) Выход.\n");
+                Console.Write("//> ");
+                choice = Console.ReadLine().First();
+
+                switch (choice)
+                {
+                    case '1':
+                        {
+                            Console.Write("Введите сообщение: ");
+                            message.Value = Console.ReadLine();
+                            Console.Write("Введите искомое слово: ");
+                            buffer = Console.ReadLine();
+                            if (message.Contains(buffer))
+                            {
+                                Console.WriteLine("Да, оно есть в сообщении.");
+                            } else
+                            {
+                                Console.WriteLine("Нет, его нет в сообщении.");
+                            }
+                            break;
+                        }
+                    case '2':
+                        {
+                            Console.Write("Введите сообщение: ");
+                            message.Value = Console.ReadLine();
+                            int fixedLength;
+                            Console.Write("Введите длину слов: ");
+                            while (!int.TryParse(Console.ReadLine(), out fixedLength)) ;
+                            if (message.GetWordsWithLength(fixedLength).Count == 0)
+                            {
+                                Console.WriteLine("Таких слов не найдено!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Найдены слова:");
+                                foreach (String word in message.GetWordsWithLength(fixedLength))
+                                {
+                                    Console.WriteLine(word);
+                                }
+                            }
+                            break;
+                        }
+                    case '3':
+                        {
+                            Console.Write("Введите сообщение: ");
+                            message.Value = Console.ReadLine();
+                            message.DeleteSingleLetterWords();
+                            Console.WriteLine("Результирующее сообщение: " + message.Value);
+                            break;
+                        }
+                    case '4':
+                        {
+                            Console.Write("Введите сообщение: ");
+                            message.Value = Console.ReadLine();
+                            message.DeleteRussianFirstVowelWords();
+                            Console.WriteLine("Результирующее сообщение: " + message.Value);
+                            break;
+                        }
+                    case '5':
+                        {
+                            Console.Write("Введите сообщение: ");
+                            message.Value = Console.ReadLine();
+                            message.ReplaceEnglishWordsByEllipsis();
+                            Console.WriteLine("Результирующее сообщение: " + message.Value);
+                            break;
+                        }
+                    case '6':
+                        {
+                            Console.Write("Введите сообщение: ");
+                            message.Value = Console.ReadLine();
+                            double sum = message.SumUpAllNumbers();
+                            Console.WriteLine("искомая сумма: " + sum);
+                            break;
+                        }
+                    case '7':
+                        {
+                            Console.Write("Введите сообщение: ");
+                            message.Value = Console.ReadLine();
+                            if (message.GetPhoneNumbers().Count == 0)
+                            {
+                                Console.WriteLine("Номеров не найдено!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Найдены номера:");
+                                foreach (String word in message.GetPhoneNumbers())
+                                {
+                                    Console.WriteLine(word);
+                                }
+                            }
+                            break;
+                        }
+                    case '8':
+                        {
+                            Console.Write("Введите сообщение: ");
+                            message.Value = Console.ReadLine();
+                            if (message.GetCurrentYearDates().Count == 0)
+                            {
+                                Console.WriteLine("Подходящих дат не найдено!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Найдены даты:");
+                                foreach (String word in message.GetCurrentYearDates())
+                                {
+                                    Console.WriteLine(word);
+                                }
+                            }
+                            break;
+                        }
+                    case '9':
+                        {
+                            Console.Write("Введите сообщение: ");
+                            message.Value = Console.ReadLine();
+                            if (message.GetWebAdresses().Count == 0)
+                            {
+                                Console.WriteLine("Адресов web-сайтов не найдено!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Найдены адреса:");
+                                foreach (String word in message.GetWebAdresses())
+                                {
+                                    Console.WriteLine(word);
+                                }
+                            }
+                            break;
+                        }
+                    case '0':
+                        {
+                            Console.Write("Введите сообщение: ");
+                            message.Value = Console.ReadLine();
+                            message.RoundSecInTime();
+                            Console.WriteLine("Результирующее сообщение: " + message.Value);
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            } while (choice != 'E');
+        }
+    }
+}
